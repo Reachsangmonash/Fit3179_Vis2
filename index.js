cb79cd2cac721f692013013450b01504e1e6429a
CHATTHATHIREACH_SANG <csan0021@student.monash.edu>
2023-10-19 06:27:50 +1100

    ..

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var map2 = "https://raw.githubusercontent.com/Reachsangmonash/Fit3179_Vis2/main/
 var bubbleGraph = "https://raw.githubusercontent.com/Reachsangmonash/Fit3179_Vis2/main/population_GDP_GDPperCapita_bubble.vg.json"
 var eodb = "https://raw.githubusercontent.com/Reachsangmonash/Fit3179_Vis2/main/ease_of_doing_business.vg.json"
 var ieportGraph = "https://raw.githubusercontent.com/Reachsangmonash/Fit3179_Vis2/main/import_export.vg.json"
-var salesProdMap = "salesProductionMapData.vg.json"
 // Access vega view
 vegaEmbed('#map01', map1).then(function (result) {
 
