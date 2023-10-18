58fd82fdb29d71f9fb688c9d8f4cdd0eba0d175a
CHATTHATHIREACH_SANG <csan0021@student.monash.edu>
2023-10-19 04:27:03 +1100

    Update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var map2 = "https://raw.githubusercontent.com/Reachsangmonash/Fit3179_Vis2/main/
 var bubbleGraph = "https://raw.githubusercontent.com/Reachsangmonash/Fit3179_Vis2/main/population_GDP_GDPperCapita_bubble.vg.json"
 var eodb = "https://raw.githubusercontent.com/Reachsangmonash/Fit3179_Vis2/main/ease_of_doing_business.vg.json"
 var ieportGraph = "https://raw.githubusercontent.com/Reachsangmonash/Fit3179_Vis2/main/import_export.vg.json"
-var marketCap2023BarGraph = "marketCap2023Graph.vg.json"
 var salesProdMap = "salesProductionMapData.vg.json"
 // Access vega view
 vegaEmbed('#map01', map1).then(function (result) {
@@ -18,9 +17,7 @@ vegaEmbed('#eodb', eodb).then(function (result) {
 vegaEmbed('#ieport', ieportGraph).then(function (result) {
 
 }).catch(console.error);
-vegaEmbed('#top10autocompany', marketCap2023BarGraph).then(function (result) {
 
-}).catch(console.error);
 vegaEmbed('#map02', map2).then(function (result) {
 
 }).catch(console.error);
